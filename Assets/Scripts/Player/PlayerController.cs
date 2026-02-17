6ab3d5cd82a593e81687fd47ba4377e30a7cdb94
NomaTyx <jjziebart99@gmail.com>
2026-02-16 16:27:14 -0800

    put food on scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,10 +3,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 50f;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _gravity = -9.81f;
 
+    [SerializeField] private float _eatRange = 5;
+    
     private float _movementX;
     private float _movementZ;
+
+    private float _groundCheckOffset = 0.1f;
 
     private CharacterController _controller;
 
@@ -23,6 +28,18 @@ public class PlayerController : MonoBehaviour
         _movementZ = movementVector.y;
     }
 
+    public void OnAttack()
+    {
+        foreach (Collider c in Physics.OverlapSphere(transform.position, _eatRange))
+        {
+            Food food = c.GetComponent<Food>();
+
+            if (food == null) continue;
+
+            food.onEaten();
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector3 forward = Camera.main.transform.forward;
@@ -34,10 +51,22 @@ public class PlayerController : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
-        Vector3 movementDir = forward * _movementZ + right * _movementX;
+        Vector3 movementDir = (forward * _movementZ + right * _movementX) * Time.deltaTime;
 
-        _controller.Move(movementDir * _moveSpeed * Time.deltaTime);
+        if (!CheckGrounded())
+        {
+            movementDir.y += _gravity * Time.deltaTime;
+            Debug.Log("not grounded");
+        }
+
+        _controller.Move(movementDir * _moveSpeed);
 
         transform.LookAt(transform.position + forward);
+    }
+
+    private bool CheckGrounded()
+    {
+        Vector3 lobsterHalfExtents = new Vector3(_controller.radius, _controller.height / 2 + _groundCheckOffset, _controller.radius);
+        return Physics.OverlapBox(_controller.center, lobsterHalfExtents).Length > 1;
     }
 }
