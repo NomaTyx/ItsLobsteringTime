12cbd3756cf3c0eaf1e7c6b21bcfce7ed3427770
NomaTyx <jjziebart99@gmail.com>
2026-02-16 20:02:36 -0800

    tweaked movement to be instant (setting velocity directly instead of adding a force)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,20 +4,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
-    [SerializeField] private float _gravity = -9.81f;
 
     [SerializeField] private float _eatRange = 5;
     
     private float _movementX;
     private float _movementZ;
 
-    private float _groundCheckOffset = 0.1f;
 
-    private CharacterController _controller;
+    private Rigidbody _rb;
 
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputValue movementValue)
@@ -51,22 +49,12 @@ public class PlayerController : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
-        Vector3 movementDir = (forward * _movementZ + right * _movementX) * Time.deltaTime;
+        Vector3 movementDir = forward * _movementZ + right * _movementX;
 
-        if (!CheckGrounded())
-        {
-            movementDir.y += _gravity * Time.deltaTime;
-            Debug.Log("not grounded");
-        }
-
-        _controller.Move(movementDir * _moveSpeed);
+        //if i do somethign like a knockback effect on the player, i will need some sort of boolean to disable 
+        _rb.linearVelocity = movementDir * _moveSpeed;
 
         transform.LookAt(transform.position + forward);
-    }
-
-    private bool CheckGrounded()
-    {
-        Vector3 lobsterHalfExtents = new Vector3(_controller.radius, _controller.height / 2 + _groundCheckOffset, _controller.radius);
-        return Physics.OverlapBox(_controller.center, lobsterHalfExtents).Length > 1;
+        Debug.Log(_rb.linearVelocity.z);
     }
 }
