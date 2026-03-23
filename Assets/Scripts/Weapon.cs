b26cbcd7d59e7f0896e9807b52e60ec4ab9f3ee1
NomaTyx <jjziebart99@gmail.com>
2026-03-22 19:50:58 -0700

    made weapon class abstract

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
     public virtual void TryAttack()
     {
         Debug.Log("Tried attack with undefined weapon.");
-        Attack(null);
     }
 
     public virtual void Attack(IAttackable target)
