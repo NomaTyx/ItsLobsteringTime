2457582fa3d35742a84fea66fbe07f985badefbe
NomaTyx <jjziebart99@gmail.com>
2026-04-26 22:45:19 -0700

    got player attacking working

--- Assets/Scripts/Player/Claw.cs
+++ Assets/Scripts/Player/Claw.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 
+/// <summary>
+/// The player's default weapon.
+/// </summary>
 public class Claw : Weapon
 {
     [SerializeField] float _attackCooldownSeconds = 0.5f;
     [SerializeField] float _attackRange = 2f;
+    [SerializeField] float _attackAngleDeg = 90f;
 
     /// <summary>
     /// Tries to attack which entails looking through its range to find a target
     /// </summary>
     public override void TryAttack()
     {
-        foreach (Collider c in Physics.OverlapSphere(transform.position, _attackRange)) 
+        foreach (Collider c in Physics.OverlapSphere(transform.position, _attackRange, LayerMask.GetMask("Enemy"))) 
         {
+            Vector3 positionDiff = c.transform.position - transform.position;
+            float angleToTarget = Vector3.Angle(transform.forward, positionDiff);
+            
+            if (angleToTarget > _attackAngleDeg / 2 || angleToTarget < -_attackAngleDeg / 2)
+            {
+                continue;
+            }
 
+            Debug.Log("Hit " + c.name + "!!");
         }
     }
 
