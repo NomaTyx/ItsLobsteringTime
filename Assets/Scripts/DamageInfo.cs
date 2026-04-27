b1135a696e29b9ecb21727696882f67eabe32905
NomaTyx <jjziebart99@gmail.com>
2026-04-26 23:00:40 -0700

    adds constructor

--- Assets/Scripts/DamageInfo.cs
+++ Assets/Scripts/DamageInfo.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class DamageInfo
 {
     public float Amount { get; private set; }
-    public float Target { get; private set; }
-    public float Instigator { get; private set; }
+    public GameObject Target { get; private set; }
+    public GameObject Instigator { get; private set; }
 
+    public DamageInfo(float amount, GameObject target, GameObject instigator)
+    {
+        Amount = amount;
+        Target = target;
+        Instigator = instigator;
+    }
 }
