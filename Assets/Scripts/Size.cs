d07497f53bedf100dd6ecb28a813e6133bb350ab
Julia Ziebart <jjziebart99@gmail.com>
2026-05-23 02:37:45 -0700

    made size enum stand on its own cuz everything is size related

---
+++ Assets/Scripts/Size.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// This enum exists just to add semantic meaning to certain words so i can avoid using magic numbers.
+/// </summary>
+public enum Size
+{
+    Small = 1,
+    Medium = 2,
+    Large = 3
+}
